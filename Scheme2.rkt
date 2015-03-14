649fd04eb50efc85edcd33aab61d65d054659f1a
Nate Kong <NateWKong@gmail.com>
2015-03-14 12:50:23 -0700

    into stuff

--- Scheme2.rkt
+++ Scheme2.rkt
@@ -1,6 +1,40 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname Scheme2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
-(define toposort 10)
+ #lang racket
+#||||||||||||||||||||||||||||||
+Topological Sort  
 
-toposort
+Team:
+Saurav Agrawal #
+Ahmed #
+Nathan Kong #7
+CS152-01
+|||||||||||||||||||||||||||||||||#
+
+;define a counter =1
+;find a vertex with zero indegree
+;assign vertex topoNum = counter
+;remove vertex and edges
+;increase counter by one
+;repeat steps 2 to 5 as long as there is a vertex with indegree
+
+
+#|||||||||||||||||||||||||
+|finds if the vertex is 0
+|||||||||||||||||||||||||#
+(define (vertex v)
+  (if (> 1 (car v)) 
+      vertex (car v)
+      
+  
+  
+
+(define (topoSort L)
+  (if (null? L) null
+      (cons (vertex (L)); returns the removed vertex
+            (topoSort (cdr L)))))
+
+       
+
+       
