db098d94fc5fa695c67864840f3c989c07e6f769
Nate Kong <NateWKong@gmail.com>
2015-03-14 14:40:39 -0700

    created list of vertices

--- Scheme2.rkt
+++ Scheme2.rkt
@@ -1,13 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname Scheme2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
- #lang racket
 #||||||||||||||||||||||||||||||
 Topological Sort  
 
-Team:
+Team: ASN Paradigm
 Saurav Agrawal #
-Ahmed #
+Ahmed Syed #
 Nathan Kong #7
 CS152-01
 |||||||||||||||||||||||||||||||||#
@@ -21,20 +20,45 @@ CS152-01
 
 
 #|||||||||||||||||||||||||
-|finds if the vertex is 0
+returns which vertex 
+has 0 indegrees
 |||||||||||||||||||||||||#
-(define (vertex v)
-  (if (> 1 (car v)) 
-      vertex (car v)
+(define (vertex v x)
+  (if (> 1 (car v)) x (vertex (cdr v) (+ x 1))))
       
-  
-  
+#|||||||||||||||||||||||||
+creates 2 list of
+numbers and indegrees
+|||||||||||||||||||||||||#
+(define (theList L)
+  (if (null? L) null
+      (cons (num L) 
+        (indegree L))))
 
+#|||||||||||||||||||||||||
+creates a list of the
+vertices
+|||||||||||||||||||||||||#
+(define (num L)
+  (if (null? L) null
+      (cons (caar L)
+            (num (cdr L)))))
+
+#||||||||||||||||||||||||
+creates a list of indegree
+||||||||||||||||||||||||#
+(define (indegree L)
+  (if (null? L) null
+      ))
+        
+  
+#||||||||||||||||||||||||
+Sorts an DAG
+Assume no infinite loops
+|||||||||||||||||||||||#
 (define (topoSort L)
   (if (null? L) null
-      (cons (vertex (L)); returns the removed vertex
-            (topoSort (cdr L)))))
-
-       
+      (cons (theList L); returns the removed vertex
+           )))
 
        
